49d4c41edee7c7a3cc25ec96a845db64f581988c
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2026-03-04 21:14:43 +0500

    Add all project files

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+     <>
+     <div className='navbar'>
+     <div></div>
+     <div></div>
+     <div></div>
+     </div>
+
+    <div>Navbar</div>
+
+   </>
+  )
+}
+
+export default Navbar
